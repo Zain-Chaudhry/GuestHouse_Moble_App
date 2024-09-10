a32b2d62295739e9b10645887461b98246d2ab33
Zain Chaudhry <zainchaudhry17302@gmail.com>
2024-09-10 05:45:08 -0700

    Updated MainActivity

--- app/src/main/java/com/example/diamondguesthouse/MainActivity.kt
+++ app/src/main/java/com/example/diamondguesthouse/MainActivity.kt
@@ -4,16 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.example.diamondguesthouse.ui.theme.DiamondGuestHouseTheme
-import com.example.diamondguesthouse.userinterface.screens.AddRecord
-import com.example.diamondguesthouse.userinterface.screens.ForgotPasswordScreen
-import com.example.diamondguesthouse.userinterface.screens.HomeScreen
-import com.example.diamondguesthouse.userinterface.screens.LoginScreen
-import com.example.diamondguesthouse.userinterface.screens.SignUp
 
 
 class MainActivity : ComponentActivity() {
@@ -23,31 +14,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             DiamondGuestHouseTheme {
                 AppNavigation()
-//                   HomeScreen()
 
                 }
             }
         }
     }
 
-object NavRoutes {
-    const val Home = "home"
-    const val Login = "login"
-    const val SignUp = "signup"
-    const val ForgotPassword = "forgot_password"
-    const val AddRecord = "add_Record"
-
-}
-
-@Composable
-fun AppNavigation() {
-    val navController = rememberNavController()
-
-    NavHost(navController = navController, startDestination = "login") {
-        composable(NavRoutes.Login) { LoginScreen(navController) }
-        composable(NavRoutes.SignUp) { SignUp(navController) }
-        composable(NavRoutes.ForgotPassword) { ForgotPasswordScreen(navController)}
-        composable(NavRoutes.Home) { HomeScreen(navController) }
-        composable(NavRoutes.AddRecord) { AddRecord(navController)}
-    }
-}
