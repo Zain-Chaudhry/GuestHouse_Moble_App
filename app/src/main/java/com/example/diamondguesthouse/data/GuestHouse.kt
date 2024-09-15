752942d14c982b4887e553d41eeb2caa91e80ea7
Zain Chaudhry <zainchaudhry17302@gmail.com>
2024-09-15 02:05:34 -0700

    Database updated

--- app/src/main/java/com/example/diamondguesthouse/data/GuestHouse.kt
+++ app/src/main/java/com/example/diamondguesthouse/data/GuestHouse.kt
@@ -5,23 +5,33 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.diamondguesthouse.data.dao.CustomerDao
+import com.example.diamondguesthouse.data.dao.RoomsDao
 import com.example.diamondguesthouse.data.model.CustomerEntity
+import com.example.diamondguesthouse.data.model.RoomEntity
 
-
-@Database(entities = [CustomerEntity::class], version = 2)
+@Database(entities = [RoomEntity::class, CustomerEntity::class], version = 3, exportSchema = false)
 abstract class GuestHouseDatabase: RoomDatabase() {
+    abstract fun roomDao(): RoomsDao
     abstract fun customerDao(): CustomerDao
 
     companion object {
         const val DATABASE_NAME = "guestHouse_database"
+
+        @Volatile
+        private var INSTANCE: GuestHouseDatabase? = null
+
         fun getDatabase(context: Context): GuestHouseDatabase {
-            return Room.databaseBuilder(
-                context,
-                GuestHouseDatabase::class.java,
-                DATABASE_NAME
-            ).build()
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    GuestHouseDatabase::class.java,
+                    DATABASE_NAME
+                )
+                    .fallbackToDestructiveMigration()  // This will allow destructive migration
+                    .build()
+                INSTANCE = instance
+                instance
+            }
         }
-
     }
-
 }
