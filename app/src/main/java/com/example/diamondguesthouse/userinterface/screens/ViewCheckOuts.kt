8cd1ea86ba283c2df50be3c648714511892b251c
Zain Chaudhry <zainchaudhry17302@gmail.com>
2024-09-24 04:59:31 -0700

    two new screen(ViewBooking and ViewCheckOuts) are added with the shared view model: ViewBookingViewModel

---
+++ app/src/main/java/com/example/diamondguesthouse/userinterface/screens/ViewCheckOuts.kt
@@ -0,0 +1,225 @@
+package com.example.diamondguesthouse.userinterface.screens
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.diamondguesthouse.R
+import com.example.diamondguesthouse.ui.theme.DiamondGuestHouseTheme
+import com.example.diamondguesthouse.viewmodel.ViewBookingViewModel
+import com.example.diamondguesthouse.viewmodel.ViewBookingViewModelFactory
+
+
+@Composable
+fun ViewCheckOuts(navController: NavController) {
+    val context = LocalContext.current
+    val viewModel: ViewBookingViewModel = ViewBookingViewModelFactory(context).create(
+        ViewBookingViewModel::class.java)
+
+    val roomsWithCustomers = viewModel.roomsWithCustomers.observeAsState(initial = emptyList())
+
+    // Get today's check-ins from the ViewModel
+    val todayCheckOuts = viewModel.getTodayCheckOuts(roomsWithCustomers.value)
+
+    Surface(modifier = Modifier.fillMaxSize()) {
+        ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+            val (topBar, nameRow, surface) = createRefs()
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_topbar),
+                contentDescription = null,
+                modifier = Modifier.constrainAs(topBar) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 80.dp, start = 16.dp, end = 16.dp)
+                    .constrainAs(nameRow) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                    }
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_arrorw_back),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .align(Alignment.CenterStart)
+                        .clickable { navController.popBackStack() }
+                )
+
+                Text(
+                    text = "Bookings",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+
+            Surface(
+                shape = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 30.dp)
+                    .constrainAs(surface) {
+                        top.linkTo(nameRow.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        bottom.linkTo(parent.bottom)
+                        height = Dimension.fillToConstraints
+                    }
+            ) {
+                if (todayCheckOuts.isEmpty()) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight()
+                            .padding(16.dp), // Add padding if necessary
+                        contentAlignment = Alignment.Center // Center content in the Box
+                    ) {
+                        Text(
+                            text = "No Check-Outs for today",
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold,
+                        )
+                    }
+
+                } else {
+                    CheckOuts(viewModel = viewModel)
+
+
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun CheckOuts( viewModel: ViewBookingViewModel) {
+    val roomsWithCustomers = viewModel.roomsWithCustomers.observeAsState(initial = emptyList())
+    val todayCheckOuts = viewModel.getTodayCheckOuts(roomsWithCustomers.value)
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        item {
+            Text(
+                text = "Today's Check-Outs:",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Room No",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.weight(1f)
+                )
+                Text(
+                    text = "Price",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.weight(1f)
+                )
+                Text(
+                    text = "Customer",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.weight(1f)
+                )
+            }
+        }
+
+        // Data Rows
+        items(todayCheckOuts) { checkOut ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = checkOut.room.roomNo,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Normal,
+                    modifier = Modifier.weight(1f)
+                )
+                Text(
+                    text = "${checkOut.room.roomPrice}",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Normal,
+                    modifier = Modifier.weight(1f)
+                )
+                if (checkOut.customers.isNotEmpty()) {
+                    Text(
+                        text = checkOut.customers[0].name,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Normal,
+                        modifier = Modifier.weight(1f)
+                    )
+                }
+            }
+        }
+        item {
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(
+                text = "Total Check Ins: ${todayCheckOuts.size}",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun ViewCheckOutPreview() {
+    DiamondGuestHouseTheme {
+        ViewCheckOuts( navController = rememberNavController())
+    }
+}
---
+++ app/src/main/java/com/example/diamondguesthouse/viewmodel/ViewBookingViewModel.kt
@@ -0,0 +1,56 @@
+package com.example.diamondguesthouse.viewmodel
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import com.example.diamondguesthouse.data.GuestHouseDatabase
+import com.example.diamondguesthouse.data.dao.RoomsDao
+import com.example.diamondguesthouse.data.model.RoomWithCustomers
+import java.text.SimpleDateFormat
+import java.util.*
+
+class ViewBookingViewModel(dao: RoomsDao): ViewModel() {
+
+    val roomsWithCustomers = dao.getAllRoomsWithCustomers().asLiveData()
+
+    // Function to get today's check-ins
+    fun getTodayCheckIns(roomsWithCustomers: List<RoomWithCustomers>): List<RoomWithCustomers> {
+        val today = getTodayDate()
+
+        // Filter the list of rooms based on today's date
+        return roomsWithCustomers.filter { roomWithCustomers ->
+            val checkInDate = Date(roomWithCustomers.room.checkInDate)
+            val formattedCheckInDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(checkInDate)
+
+            formattedCheckInDate == today
+        }  // Return empty list if no check-ins for today
+    }
+
+    fun getTodayCheckOuts(roomsWithCustomers: List<RoomWithCustomers>): List<RoomWithCustomers> {
+        val today = getTodayDate()
+
+        // Filter the list of rooms based on today's date
+        return roomsWithCustomers.filter { roomWithCustomers ->
+            val checkOutDate = Date(roomWithCustomers.room.checkOutDate)
+            val formattedCheckInDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(checkOutDate)
+
+            formattedCheckInDate == today
+        }  // Return empty list if no check-ins for today
+    }
+
+    // Utility function to get today's date as a formatted string
+    private fun getTodayDate(): String {
+        val today = Calendar.getInstance().time
+        return SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(today)
+    }
+}
+
+
+class ViewBookingViewModelFactory(private val context: Context): ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val dao = GuestHouseDatabase.getDatabase(context).roomDao()
+        @Suppress ("UNCHECKED_CAST")
+        return ViewBookingViewModel(dao) as T
+    }
+}
