8f6c3a70ce5f7a67a6fad64e0e2eefa56e4b65c1
Zain Chaudhry <zainchaudhry17302@gmail.com>
2024-09-24 04:44:58 -0700

    version updated

--- app/src/main/java/com/example/diamondguesthouse/data/GuestHouse.kt
+++ app/src/main/java/com/example/diamondguesthouse/data/GuestHouse.kt
@@ -9,7 +9,7 @@ import com.example.diamondguesthouse.data.dao.RoomsDao
 import com.example.diamondguesthouse.data.model.CustomerEntity
 import com.example.diamondguesthouse.data.model.RoomEntity
 
-@Database(entities = [RoomEntity::class, CustomerEntity::class], version = 3, exportSchema = false)
+@Database(entities = [RoomEntity::class, CustomerEntity::class], version = 4, exportSchema = false)
 abstract class GuestHouseDatabase: RoomDatabase() {
     abstract fun roomDao(): RoomsDao
     abstract fun customerDao(): CustomerDao
