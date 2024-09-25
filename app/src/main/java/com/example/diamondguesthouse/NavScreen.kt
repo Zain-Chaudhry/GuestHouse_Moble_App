b729c8b9622a3018d49520afdf960287fb15796f
Zain Chaudhry <zainchaudhry17302@gmail.com>
2024-09-25 04:33:27 -0700

    new paths are added to NavScreen.kt

--- app/src/main/java/com/example/diamondguesthouse/NavScreen.kt
+++ app/src/main/java/com/example/diamondguesthouse/NavScreen.kt
@@ -5,28 +5,30 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.diamondguesthouse.NavRoutes.BookingBySearch
 import com.example.diamondguesthouse.userinterface.screens.AddRecord
 import com.example.diamondguesthouse.userinterface.screens.BookingBySearch
 import com.example.diamondguesthouse.userinterface.screens.ForgotPasswordScreen
 import com.example.diamondguesthouse.userinterface.screens.HomeScreen
 import com.example.diamondguesthouse.userinterface.screens.LoginScreen
 import com.example.diamondguesthouse.userinterface.screens.SearchRecord
+import com.example.diamondguesthouse.userinterface.screens.SettingScreen
 import com.example.diamondguesthouse.userinterface.screens.SignUp
 import com.example.diamondguesthouse.userinterface.screens.ViewBooking
 import com.example.diamondguesthouse.userinterface.screens.ViewCheckOuts
+import com.example.diamondguesthouse.viewmodel.AuthViewModel
 import com.example.diamondguesthouse.viewmodel.SearchRecordViewModel
 
 object NavRoutes {
-    const val Home = "home"
-    const val Login = "login"
-    const val SignUp = "signup"
-    const val ForgotPassword = "forgot_password"
-    const val AddRecord = "add_Record"
-    const val SearchRecord = "search_Record"
-    const val BookingBySearch = "booking_by_search"
-    const val ViewBooking = "view_booking"
-    const val ViewCheckOut = "view_checkout"
+    const val HOME = "home"
+    const val LOGIN = "login"
+    const val SIGNUP = "signup"
+    const val FORGOT_PASSWORD = "forgot_password"
+    const val ADD_RECORD = "add_Record"
+    const val SEARCH_RECORD = "search_Record"
+    const val BOOKING_BY_SEARCH = "booking_by_search"
+    const val VIEW_BOOKING = "view_booking"
+    const val VIEW_CHECKOUT = "view_checkout"
+    const val SETTING_SCREEN = "settings_screen"
 
 }
 
@@ -34,17 +36,19 @@ object NavRoutes {
 fun AppNavigation() {
     val navController = rememberNavController()
     val sharedViewModel: SearchRecordViewModel = viewModel()
+    val authViewModel: AuthViewModel = viewModel()
 
-    NavHost(navController = navController, startDestination = "login") {
-        composable(NavRoutes.Login) { LoginScreen(navController) }
-        composable(NavRoutes.SignUp) { SignUp(navController) }
-        composable(NavRoutes.ForgotPassword) { ForgotPasswordScreen(navController) }
-        composable(NavRoutes.Home) { HomeScreen(navController) }
-        composable(NavRoutes.AddRecord) { AddRecord(navController) }
-        composable(NavRoutes.SearchRecord) { SearchRecord(navController, sharedViewModel) }
-        composable(BookingBySearch) { BookingBySearch(sharedViewModel, navController)}
-        composable(NavRoutes.ViewBooking) { ViewBooking( navController = navController) }
-        composable(NavRoutes.ViewCheckOut) { ViewCheckOuts( navController = navController) }
+    NavHost(navController = navController, startDestination = NavRoutes.LOGIN) {
+        composable(NavRoutes.LOGIN) { LoginScreen(authViewModel, navController) }
+        composable(NavRoutes.SIGNUP) { SignUp(authViewModel, navController) }
+        composable(NavRoutes.FORGOT_PASSWORD) { ForgotPasswordScreen(authViewModel) }
+        composable(NavRoutes.HOME) { HomeScreen(navController) }
+        composable(NavRoutes.ADD_RECORD) { AddRecord(navController) }
+        composable(NavRoutes.SEARCH_RECORD) { SearchRecord(navController, sharedViewModel) }
+        composable(NavRoutes.BOOKING_BY_SEARCH) { BookingBySearch(sharedViewModel, navController)}
+        composable(NavRoutes.VIEW_BOOKING) { ViewBooking( navController = navController) }
+        composable(NavRoutes.VIEW_CHECKOUT) { ViewCheckOuts( navController = navController) }
+        composable(NavRoutes.SETTING_SCREEN) { SettingScreen(authViewModel, navController) }
 
 
     }
