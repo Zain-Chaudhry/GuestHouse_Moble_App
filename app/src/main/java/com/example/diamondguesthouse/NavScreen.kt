6f62b63907fa7b77e2c79036d413ccfb5a3c28c4
Zain Chaudhry <zainchaudhry17302@gmail.com>
2024-09-30 05:00:55 -0700

    new routes added

--- app/src/main/java/com/example/diamondguesthouse/NavScreen.kt
+++ app/src/main/java/com/example/diamondguesthouse/NavScreen.kt
@@ -10,11 +10,13 @@ import com.example.diamondguesthouse.userinterface.screens.BookingBySearch
 import com.example.diamondguesthouse.userinterface.screens.ForgotPasswordScreen
 import com.example.diamondguesthouse.userinterface.screens.HomeScreen
 import com.example.diamondguesthouse.userinterface.screens.LoginScreen
+import com.example.diamondguesthouse.userinterface.screens.ReportScreen
 import com.example.diamondguesthouse.userinterface.screens.SearchRecord
 import com.example.diamondguesthouse.userinterface.screens.SettingScreen
 import com.example.diamondguesthouse.userinterface.screens.SignUp
 import com.example.diamondguesthouse.userinterface.screens.ViewBooking
 import com.example.diamondguesthouse.userinterface.screens.ViewCheckOuts
+import com.example.diamondguesthouse.viewmodel.AddRecordViewModel
 import com.example.diamondguesthouse.viewmodel.AuthViewModel
 import com.example.diamondguesthouse.viewmodel.SearchRecordViewModel
 
@@ -29,6 +31,7 @@ object NavRoutes {
     const val VIEW_BOOKING = "view_booking"
     const val VIEW_CHECKOUT = "view_checkout"
     const val SETTING_SCREEN = "settings_screen"
+    const val REPORT_SCREEN = "report_screen"
 
 }
 
@@ -37,18 +40,20 @@ fun AppNavigation() {
     val navController = rememberNavController()
     val sharedViewModel: SearchRecordViewModel = viewModel()
     val authViewModel: AuthViewModel = viewModel()
+    val addRecordViewModel: AddRecordViewModel = viewModel()
 
     NavHost(navController = navController, startDestination = NavRoutes.LOGIN) {
         composable(NavRoutes.LOGIN) { LoginScreen(authViewModel, navController) }
         composable(NavRoutes.SIGNUP) { SignUp(authViewModel, navController) }
         composable(NavRoutes.FORGOT_PASSWORD) { ForgotPasswordScreen(authViewModel) }
         composable(NavRoutes.HOME) { HomeScreen(navController) }
-        composable(NavRoutes.ADD_RECORD) { AddRecord(navController) }
+        composable(NavRoutes.ADD_RECORD) { AddRecord(addRecordViewModel, navController) }
         composable(NavRoutes.SEARCH_RECORD) { SearchRecord(navController, sharedViewModel) }
         composable(NavRoutes.BOOKING_BY_SEARCH) { BookingBySearch(sharedViewModel, navController)}
         composable(NavRoutes.VIEW_BOOKING) { ViewBooking( navController = navController) }
         composable(NavRoutes.VIEW_CHECKOUT) { ViewCheckOuts( navController = navController) }
         composable(NavRoutes.SETTING_SCREEN) { SettingScreen(authViewModel, navController) }
+        composable(NavRoutes.REPORT_SCREEN) { ReportScreen(navController) }
 
 
     }
