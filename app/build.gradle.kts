7570719cb5acbb218daf0acadf88690bbb94d183
Zain Chaudhry <zainchaudhry17302@gmail.com>
2024-10-03 06:22:27 -0700

    splash screen dependency added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,7 @@ dependencies {
     implementation(libs.androidx.graphics.core)
     implementation(libs.androidx.graphics.path)
     implementation(libs.androidx.graphics.shapes)
+    implementation(libs.androidx.core.splashscreen)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
